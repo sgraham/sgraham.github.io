83678fe4ad998f944300492f68b0e41a0d6dc7fe
Scott Graham <scott.github@h4ck3r.net>
2020-10-18 19:28:52 -0700

    fiddle with keep_on 3

--- recipes/keep_on.js
+++ recipes/keep_on.js
@@ -1,45 +1,31 @@
 (function(){
-var fullScreenButton = document.createElement("button");
-fullScreenButton.innerText = 'Enter Full Screen';
-document.body.insertBefore(fullScreenButton, document.body.childNodes[0]);
-
-fullScreenButton.addEventListener('click', () => {
-  if (!document.fullscreenElement) {
-    document.documentElement.requestFullscreen();
-    fullScreenButton.textContent = 'Leave Full Screen';
-  } else {
-    document.exitFullscreen();
-    fullScreenButton.textContent = 'Enter Full Screen';
-  }  
-});
-
-if ('wakeLock' in navigator && 'request' in navigator.wakeLock) {  
+if ('wakeLock' in navigator && 'request' in navigator.wakeLock) {
   let wakeLock = null;
-  
+
   const requestWakeLock = async () => {
     try {
       wakeLock = await navigator.wakeLock.request('screen');
       wakeLock.addEventListener('release', (e) => {
         console.log(e);
-        console.log('Wake Lock was released');                    
+        console.log('Wake Lock was released');
       });
-      console.log('Wake Lock is active');      
-    } catch (e) {      
+      console.log('Wake Lock is active');
+    } catch (e) {
       console.error(`${e.name}, ${e.message}`);
-    } 
+    }
   };
-  
+
   requestWakeLock();
-  
-  const handleVisibilityChange = () => {    
+
+  const handleVisibilityChange = () => {
     if (wakeLock !== null && document.visibilityState === 'visible') {
       requestWakeLock();
     }
-  };    
-  
+  };
+
   document.addEventListener('visibilitychange', handleVisibilityChange);
   document.addEventListener('fullscreenchange', handleVisibilityChange);
-} else {  
+} else {
   console.error('Wake Lock API not supported.');
 }
 })();
